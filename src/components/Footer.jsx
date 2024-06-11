f3b6f8ecc86f5699f59473f20a03b65e2ec79e20
Bouchghel <bouchghelismail@gmail.com>
2024-06-11 21:49:05 +0100

    fr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,30 +7,30 @@ const Footer = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-6 col-xl-3 col-sm-12 mb-4 mb-xl-0">
-              <h5 className="pb-3"><i className="fa-solid fa-user-group pe-1"></i> About us</h5>
-              <span className="text-secondary">This is a wider card with supporting text below as a natural lead-in to additional content.</span>
+              <h5 className="pb-3"><i className="fa-solid fa-user-group pe-1"></i> À propos de nous</h5>
+              <span className="text-secondary">Ceci est une carte plus large avec du texte d'accompagnement ci-dessous comme introduction naturelle à un contenu supplémentaire.</span>
             </div>
             <div className="col-md-6 col-xl-3 col-sm-12 mb-4 mb-xl-0">
-              <h5 className="pb-3"><i className="fa-solid fa-link pe-1"></i> Important links</h5>
+              <h5 className="pb-3"><i className="fa-solid fa-link pe-1"></i> Liens importants</h5>
               <ul className="list-unstyled">
-                <li><a href="#" className="text-decoration-none link-secondary">About us</a></li>
-                <li><a href="#" className="text-decoration-none link-secondary">Privacy policy</a></li>
-                <li><a href="#" className="text-decoration-none link-secondary">Terms of services</a></li>
+                <li><a href="#" className="text-decoration-none link-secondary">À propos de nous</a></li>
+                <li><a href="#" className="text-decoration-none link-secondary">Politique de confidentialité</a></li>
+                <li><a href="#" className="text-decoration-none link-secondary">Conditions de service</a></li>
               </ul>
             </div>
             <div className="col-md-6 col-xl-3 col-sm-12 mb-4 mb-md-0">
-              <h5 className="pb-3"><i className="fa-solid fa-location-dot pe-1"></i> Our location</h5>
+              <h5 className="pb-3"><i className="fa-solid fa-location-dot pe-1"></i> Notre emplacement</h5>
               <span className="text-secondary">
-                Tangier<br />
+                Tanger<br />
                 z.i ghzenaya lot 115-117-119 <br />
                 90000, Maroc<br />
               </span>
             </div>
             <div className="col-md-6 col-xl-3 col-sm-12">
-              <h5 className="pb-3"><i className="fa-solid fa-paper-plane pe-1"></i> Stay updated</h5>
+              <h5 className="pb-3"><i className="fa-solid fa-paper-plane pe-1"></i> Restez informé</h5>
               <form>
-                <input type="text" className="w-100 mb-2 form-control" placeholder="Email ID" />
-                <button className="w-100 btn btn-dark">Subscribe now</button>
+                <input type="text" className="w-100 mb-2 form-control" placeholder="Adresse e-mail" />
+                <button className="w-100 btn btn-dark">Abonnez-vous maintenant</button>
               </form>
             </div>
           </div>
@@ -49,7 +49,7 @@ const Footer = () => {
               </ul>
             </div>
             <div className="col-md-6 col-sm-12">
-              <span className="text-secondary pt-1 d-md-flex justify-content-md-end d-sm-flex justify-content-sm-start">Copyright &copy; 2024</span>
+              <span className="text-secondary pt-1 d-md-flex justify-content-md-end d-sm-flex justify-content-sm-start">Droits d'auteur &copy; 2024</span>
             </div>
           </div>
         </div>
